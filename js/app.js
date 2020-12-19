b3a2a8244370727c014719c4fcdfe3c628b5e67e
shreyas1hack <shreyasmohite11123@gmail.com>
2020-12-19 08:34:38 +0530

    javascript

---
+++ js/app.js
@@ -0,0 +1,25 @@
+import * as moti from "./quotes.js";
+
+
+
+const but = document.getElementById("but");
+window.onload = () => {
+    but.addEventListener("click", () => {
+        let quote = document.getElementById("quotes");
+        let author = document.getElementById("author");
+        let index = parseInt(Math.random() * 48);
+        console.log(index);
+
+        let get_quotes = moti.motivation[index].quote;
+        let get_author = moti.motivation[index].author;
+        quote.innerHTML = get_quotes;
+        author.innerHTML = "---" + get_author;
+
+
+    });
+
+}
+
+
+
+//let index = parseInt(Math.random * 48);
---
+++ js/quotes.js
@@ -0,0 +1,195 @@
+export let motivation = [{
+        quote: "All our dreams can come true, if we have the courage to pursue them.",
+        author: "Walt Disney"
+    },
+    {
+        quote: "The secret of getting ahead is getting started.",
+        author: "Mark Twain",
+
+    },
+    {
+        quote: "Don't limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.",
+        author: "Mary Kay Ash",
+    },
+    {
+        quote: "I wake up every morning and think to myself, how far can I push this company in the next 24 hours.",
+        author: "Leah Busque",
+    },
+    {
+        quote: "Everything you can imagine is real",
+        author: "Pablo Picasso"
+    },
+    {
+        quote: "Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten",
+        author: "Neil Gaiman"
+    },
+    {
+        quote: "Do what you feel in your heart to be right - for you'll be criticized anyway.",
+        author: "Socrates"
+    },
+    {
+        quote: "Whatever you are, be a good one.",
+        author: "Abraham Lincoln"
+    },
+    {
+        quote: "Happiness is not something ready made. It comes from your own actions",
+        author: "Dalai Lama XIV"
+    },
+    {
+        quote: "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+        author: "Aristotle"
+    },
+    {
+        quote: "If you're offered a seat on a rocket ship, don't ask what seat! Just get on.",
+        author: "Sheryl Sandberg"
+    },
+    {
+        quote: "We can see through others only when we can see through ourselves.",
+        author: "Bruce Lee"
+    },
+    {
+        quote: "Nothing ever goes away until it teaches us what we need to know.",
+        author: "Pema Chodron"
+    },
+    {
+        quote: "The two most important days in your life are the day you're born and the day you find out why.",
+        author: "Mark Twain"
+    },
+    {
+        quote: "Once you do know what the question actually is, you'll know what the answer means",
+        author: "Douglas Adams"
+    },
+    {
+        quote: "If you judge people, you have no time to love them.",
+        author: "Mother Teresa"
+    },
+    {
+        quote: "If you've never eaten while crying you don't know what life tastes like.",
+        author: "Johann Wolfgang von Goethe"
+    },
+    {
+        quote: "When you know your worth, no one can make you feel worthless.",
+        author: " Unknown"
+    },
+    {
+        quote: "What hurts you blesses you.",
+        author: "Rumi"
+    },
+    {
+        quote: "Today is your opportunity to build the tomorrow you want",
+        author: "Ken Poirot"
+    },
+    {
+        quote: "If your dreams don't scare you, they are too small",
+        author: "Richard Branson"
+    }, {
+        quote: "Done is better than perfect.",
+        author: "Sheryl Sandberg"
+    },
+    {
+        quote: "I was smart enough to go through any door that opened.",
+        author: "Joan Rivers"
+    },
+    {
+        quote: "Learn from the mistakes of others. You can't live long enough to make them all yourself.",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        quote: "Be a first rate version of yourself, not a second rate version of someone else.",
+        author: "Judy Garland"
+    },
+    {
+        quote: "Doubt is a killer. You just have to know who you are and what you stand for. ",
+        author: "Jennifer Lopez"
+    },
+    {
+        quote: "When something I can't control happens, I ask myself: Where is the hidden gift? Where is the positive in this?",
+        author: "Sara Blakely"
+    },
+    {
+        quote: "Do one thing every day that scares you.",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        quote: "A goal is a dream with a deadline.",
+        author: "Napoleon Hill"
+    },
+    {
+        quote: "I never dreamt of success. I worked for it.",
+        author: "Estee Lauder"
+    },
+    {
+        quote: "Action is the foundational key to all success.",
+        author: "Pablo Picasso"
+    },
+    {
+        quote: "If opportunity doesn't knock build a door",
+        author: "Milton Berle"
+    },
+    {
+        quote: "It's never too late for a new beginning in your life.",
+        author: "Joyce Meyers"
+    },
+    {
+        quote: "If there is no wind, row.",
+        author: "Latin Proverb"
+    },
+    {
+        quote: "It's not your salary that makes you rich, it's your spending habits.",
+        author: "Charles A. Jaffe"
+    },
+    {
+        quote: "Quitters never win. Winners never quit!",
+        author: "Dr. Irene C. Kassorla"
+    },
+    {
+        quote: "What's on the other side of fear? Nothing.",
+        author: "Jamie Foxx"
+    },
+    {
+        quote: "The best revenge is massive success.",
+        author: "Frank Sinatra"
+    },
+    {
+        quote: "I want to be remembered as the one who tried.",
+        author: "Dr. Dorothy Height"
+    },
+    {
+        quote: "The key to success is to start before you are ready.",
+        author: "Marie Forleo"
+    },
+    {
+        quote: "Forget your excuses. You either want it bad or don't want it at all.",
+        author: "Unknown"
+    },
+    {
+        quote: "If you talk about it, it's a dream. If you envision it, it's possible. If you schedule it, it's real.",
+        author: "Tony Robbins"
+    },
+    {
+        quote: "Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do",
+        author: "Pele"
+    },
+    {
+        quote: "Your positive action combined with positive thinking results in success.",
+        author: "Shiv Khera"
+    },
+    {
+        quote: "The secret of change is to focus all your energy, not on fighting the old, but on building the new.",
+        author: "Socrates"
+    },
+    {
+        quote: "Don't be upset when people reject you. Nice things are rejected all the time by people who can't afford them.",
+        author: "Unknown"
+    },
+    {
+        quote: "Don't give up, don't take anything personally, and don't take no for an answer.",
+        author: "Sophia Amoruso"
+    },
+
+    {
+        quote: "One day our body will be use less if we work less on our dreams.",
+        author: "Shreyas Mohite"
+    }
+
+]
